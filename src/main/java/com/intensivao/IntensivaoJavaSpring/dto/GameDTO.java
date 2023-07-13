843a0aa25d02e77878b45f1a4e1391769e405a26
jcorreaw <juninhocorrea2014@gmail.com>
2023-07-13 10:45:24 -0300

    GET game by id

--- src/main/java/com/intensivao/IntensivaoJavaSpring/dto/GameDTO.java
+++ src/main/java/com/intensivao/IntensivaoJavaSpring/dto/GameDTO.java
@@ -1,43 +1,27 @@
-package com.intensivao.IntensivaoJavaSpring.entities;
+package com.intensivao.IntensivaoJavaSpring.dto;
 
-import jakarta.persistence.*;
+import com.fasterxml.jackson.databind.util.BeanUtil;
+import com.intensivao.IntensivaoJavaSpring.entities.Game;
+import jakarta.persistence.Column;
+import org.springframework.beans.BeanUtils;
 
-import java.util.Objects;
-
-@Entity
-@Table(name = "tb_game")
-public class Game {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+public class GameDTO {
     private Long id;
     private String title;
-    @Column(name = "game_year")
     private Integer year;
     private String genre;
     private String platforms;
     private Double score;
     private String imgUrl;
-    @Column(columnDefinition = "TEXT")
     private String shortDescription;
-    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
-    public Game() {
+    public GameDTO() {
     }
 
-    public Game(Long id, String title, Integer year, String genre, String platform, Double score, String imgUrl, String shortDescription, String longDescription) {
-        this.id = id;
-        this.title = title;
-        this.year = year;
-        this.genre = genre;
-        this.platforms = platform;
-        this.score = score;
-        this.imgUrl = imgUrl;
-        this.shortDescription = shortDescription;
-        this.longDescription = longDescription;
+    public GameDTO(Game entity) {
+        BeanUtils.copyProperties(entity, this);
     }
-
 
     public Long getId() {
         return id;
@@ -59,14 +43,6 @@ public class Game {
         return year;
     }
 
-    public Double getScore() {
-        return score;
-    }
-
-    public void setScore(Double score) {
-        this.score = score;
-    }
-
     public void setYear(Integer year) {
         this.year = year;
     }
@@ -83,8 +59,16 @@ public class Game {
         return platforms;
     }
 
-    public void setPlatforms(String platform) {
-        this.platforms = platform;
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
     }
 
     public String getImgUrl() {
@@ -109,18 +93,5 @@ public class Game {
 
     public void setLongDescription(String longDescription) {
         this.longDescription = longDescription;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Game game = (Game) o;
-        return Objects.equals(id, game.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
     }
 }
--- src/main/java/com/intensivao/IntensivaoJavaSpring/services/GameService.java
+++ src/main/java/com/intensivao/IntensivaoJavaSpring/services/GameService.java
@@ -1,10 +1,12 @@
 package com.intensivao.IntensivaoJavaSpring.services;
 
+import com.intensivao.IntensivaoJavaSpring.dto.GameDTO;
 import com.intensivao.IntensivaoJavaSpring.dto.GameMinDTO;
 import com.intensivao.IntensivaoJavaSpring.entities.Game;
 import com.intensivao.IntensivaoJavaSpring.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 @Service
@@ -13,6 +15,15 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
+    }
+
+
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
         return result.stream().map(GameMinDTO::new).toList();
